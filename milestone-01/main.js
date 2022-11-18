502eacd20a7737222173b7ad2a7dabeeace110bd
federico697 <federico697697@gmail.com>
2022-11-18 11:29:02 +0100

    fine milestone 1 e 2, manca 3 e 4

---
+++ milestone-01/main.js
@@ -0,0 +1,95 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        contatti: [{
+            name: 'Federico',
+            avatar: '_1',
+            visible: true,
+            messagges: [{
+                date:'10/01/2020 15:30:55',
+                messagge: 'Hai portato a spasso il cane?',
+                status: 'sent'
+            },
+            {
+                date:'10/01/2020 15:30:55',
+                messagge: 'ricordati di stendere i panni',
+                status: 'sent'
+            },
+            {
+                date:'10/01/2020 15:30:55',
+                messagge: 'Tutto fatto!!',
+                status: 'recived'
+            }
+        ],
+      },
+      {
+        name: 'Marco',
+            avatar: '_2',
+            visible: true,
+            messagges: [{
+                date:'10/01/2020 15:30:55',
+                messagge: 'Hai portato a spasso il cane?',
+                status: 'sent'
+            },
+            {
+                date:'10/01/2020 15:30:55',
+                messagge: 'ricordati di stendere i panni',
+                status: 'sent'
+            },
+            {
+                date:'10/01/2020 15:30:55',
+                messagge: 'Tutto fatto!!',
+                status: 'recived'
+            }
+        ],
+      },
+      {
+        name: 'Alessio',
+            avatar: '_3',
+            visible: true,
+            messagges: [{
+                date:'10/01/2020 15:30:55',
+                messagge: 'Hai portato a spasso il cane?',
+                status: 'sent'
+            },
+            {
+                date:'10/01/2020 15:30:55',
+                messagge: 'ricordati di stendere i panni',
+                status: 'sent'
+            },
+            {
+                date:'10/01/2020 15:30:55',
+                messagge: 'Tutto fatto!!',
+                status: 'recived'
+            }
+        ],
+      },
+      {
+        name: 'Luca',
+            avatar: '_4',
+            visible: true,
+            messagges: [{
+                date:'10/01/2020 15:30:55',
+                messagge: 'Hai portato a spasso il cane?',
+                status: 'sent'
+            },
+            {
+                date:'10/01/2020 15:30:55',
+                messagge: 'ricordati di stendere i panni',
+                status: 'sent'
+            },
+            {
+                date:'10/01/2020 15:30:55',
+                messagge: 'Tutto fatto!!',
+                status: 'recived'
+            }
+        ],
+      }
+    ]
+    },
+  
+    methods: {
+
+      }
+    }
+  )
